f0dddea2d2fdaf43b70e4cc42947f2df21225d36
Natali Zikrach <mne310872@yandex.ru>
2024-03-05 11:38:00 +0300

    init: init task

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div></div>
+    </>
+  );
+}
+
+export default App;
